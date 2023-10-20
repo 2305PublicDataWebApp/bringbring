f02b87de2804d9cc171796d9a541f33520d77491
ktweb5951 <ktweb5951@gmail.com>
2023-10-20 12:00:26 +0900

    [FIX] 23/10/20 (11:59)
    1. 머지 오류 수정

--- src/main/java/com/bringbring/inquire/service/InquireService.java
+++ src/main/java/com/bringbring/inquire/service/InquireService.java
@@ -13,12 +13,14 @@ public interface InquireService {
 
     /**
      * 문의굴 갯수 조회 service
+     *
      * @return int
      */
     int selectInqListCount();
 
     /**
      * 문의굴 리스트 조회 service
+     *
      * @param pInfo
      * @return List<Inquire>
      */
@@ -35,25 +37,29 @@ public interface InquireService {
 
     /**
      * 지역별 문의 리스트 조회 service
+     *
      * @param inquireDetails
-     * @return  List<InquireDetails>
+     * @return List<InquireDetails>
      */
     List<InquireDetails> selectInquireByCityNo(InquireDetails inquireDetails, PageInfo pageInfo);
 
     /**
      * 답변 여부 글 수 조회 service
+     *
      * @return int
      */
     int selectInquireCount(char answerYn);
 
     /**
      * 지역별 글 수 조회 service
+     *
      * @return int
      */
     int selectInquireCountByCity(int cityNo);
 
     /**
      * 키워드로 문의글 검색 service
+     *
      * @param pInfo
      * @param paramMap
      * @return List<InquireDetails>
@@ -62,6 +68,7 @@ public interface InquireService {
 
     /**
      * 키워드 문의글 갯수 검색 service
+     *
      * @param paramMap
      * @return
      */
@@ -77,9 +84,11 @@ public interface InquireService {
 //    int insertInquire(Inquire inquire, MultipartFile[] uploadFiles, HttpServletRequest request);
 
     /**
-	  * 회원별 문의내역 리스트 service
-	  * @param userNo
-	  * @return Inquire
-	  */
-	  List<Inquire> selectInquireListByUserNo(int userNo);
+     * 회원별 문의내역 리스트 service
+     *
+     * @param userNo
+     * @return Inquire
+     */
+    List<Inquire> selectInquireListByUserNo(int userNo);
+}
 
